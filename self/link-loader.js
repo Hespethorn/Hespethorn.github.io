282dd3ad76b45546ed0c46f596deabb33fee5063
Hespethorn <17362995493@163.com>
2026-03-12 10:00:34 +0800

    Site updated: 2026-03-12 09:57:06

---
+++ self/link-loader.js
@@ -0,0 +1,29 @@
+/**
+ * 链接加载器模块
+ * 处理中英文链接的跳转行为
+ */
+(() => {
+  const isIncludeEN = item => {
+    const key = '/en/'
+    return item.includes(key)
+  }
+
+  window.loadFullPage = (url) => {
+    window.location.href = url
+  }
+
+  const eventFn = (elements, includeEN) => {
+    elements.forEach(item => {
+      if (!includeEN || !isIncludeEN(item.href)) {
+        item.href = `javascript:loadFullPage('${item.href}');`
+      }
+    })
+  }
+
+  const nowIncludeEN = isIncludeEN(window.location.href)
+  const selector = nowIncludeEN
+    ? document.querySelectorAll('a[href^="https://butterfly.js.org"]')
+    : document.querySelectorAll('a[href^="/en/"]')
+
+  eventFn(selector, nowIncludeEN)
+})()
